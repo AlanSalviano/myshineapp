76a51acd8f85ed97e6fb2d1e187ec42e19cc50d3
AlanSalviano <alanqms@gmail.com>
2025-09-16 00:45:28 -0300

    Add files via upload

---
+++ public/analytics.js
@@ -0,0 +1,5 @@
+// public/analytics.js
+document.addEventListener('DOMContentLoaded', () => {
+    // Lógica para a página de análises pode ser adicionada aqui
+    console.log('Página de Análises carregada.');
+});
